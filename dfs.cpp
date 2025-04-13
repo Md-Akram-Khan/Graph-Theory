ee2f637730721b186f038c56436defa16905fced
Thank_you <74777405+Md-Akram-Khan@users.noreply.github.com>
2025-04-13 23:50:32 +0600

    Update dfs.cpp

--- dfs.cpp
+++ dfs.cpp
@@ -90,3 +90,53 @@ input:
 output:
 DFS traversal: 7 3 1 2 6 10 5 8 9 4
 */
+/*
+import java.util.*;
+
+public class Graph {
+    public static void dfs(int parent, boolean[] vis,
+                           ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> ls) {
+        vis[parent] = true;       // Mark as visited
+        ls.add(parent);           // Add to result list
+
+        for (int child : adj.get(parent)) {
+            if (!vis[child]) {
+                dfs(child, vis, adj, ls);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int node = sc.nextInt();
+        int edge = sc.nextInt();
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < node; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < edge; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj.get(u).add(v);
+            adj.get(v).add(u); // for undirected graph
+        }
+
+        boolean[] vis = new boolean[node];
+        ArrayList<Integer> ls = new ArrayList<>();
+
+        dfs(0, vis, adj, ls); // Start DFS from node 0
+
+        // Print the result
+        System.out.println("DFS Traversal:");
+        for (int val : ls) {
+            System.out.print(val + " ");
+        }
+
+        sc.close();
+    }
+}
+
+*/
